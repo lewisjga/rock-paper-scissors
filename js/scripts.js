ce9dc47e2bac2e3c737bfc9a4cab48bdf0f05173
Garrett Lewis <115313310+lewisjga@users.noreply.github.com>
2024-02-26 17:17:04 -0600

    Externalize js

---
+++ js/scripts.js
@@ -0,0 +1,147 @@
+//randomly generates a selection of either rock, scissors, or paper to be compared to player choice
+
+function getComputerChoice(){
+    let choice = ''
+    let choiceCode = Math.floor(Math.random()*3);
+    console.log(choiceCode);
+    switch(choiceCode) {
+        case 0:
+            choice = 'rock';
+            break;
+        case 1:
+            choice = 'scissors';
+            break;
+        case 2:
+            choice = 'paper';
+            break;
+        default:
+            console.log('Your code sucks');
+            break;
+    }
+    return(choice); 
+}
+
+//compares the choices of the player against the choices of the computer to determine the outcome of a match
+
+function playBall(choicePly, choiceCpu){
+    choicePly = choicePly.toLowerCase();
+    choiceCpu = choiceCpu.toLowerCase();
+    let gameEndMsg = '';
+    let victor = '';
+    if (choicePly === 'rock') {
+        switch(choiceCpu) {
+            case 'rock':
+                gameEndMsg = "It's a tie. You both picked rock.";
+                victor = "tie";
+                break;
+            case 'scissors':
+                gameEndMsg = "Rock beats scissors, you win!";
+                victor = "player";
+                break;
+            case 'paper':
+                gameEndMsg = "Paper beats rock, you lose!";
+                victor = "computer";
+                break;
+            default:
+                console.log("You messed up.");
+                gameEndMsg = "oops";
+                break;
+        }
+    }
+    else if (choicePly === 'scissors') {
+        switch(choiceCpu) {
+            case 'rock':
+                gameEndMsg = "Rock beats scissors, you lose!";
+                victor = "computer";
+                break;
+            case 'scissors':
+                gameEndMsg = "It's a tie. You both picked scissors.";
+                victor = "tie";
+                break;
+            case 'paper':
+                gameEndMsg = "Scissors beats paper, you win!";
+                victor = "player";
+                break;
+            default:
+                console.log("You messed up.");
+                gameEndMsg = "oops";
+                break;
+        }
+    } else if (choicePly === 'paper') {
+        switch(choiceCpu) {
+            case 'rock':
+                gameEndMsg = "Paper beats rock, you win!";
+                victor = "player";
+                break;
+            case 'scissors':
+                gameEndMsg = "Scissors beats paper, you lose!";
+                victor = "computer";
+                break;
+            case 'paper':
+                gameEndMsg = "It's a tie. You both picked paper.";
+                victor = "tie";
+                break;
+            default:
+                console.log("You messed up.");
+                gameEndMsg = "oops";
+                break;
+        }
+    } else {
+        console.log("The player did not choose a valid option.")
+    }
+    return[gameEndMsg, victor];
+}
+
+//Plays game through five times and declares a victor.
+
+/*
+function playThrough(playerSelection){
+    let playerScore = 0;
+    let compScore = 0;
+
+    //game loop and scoring mechanism
+
+    for (let i = 0; i < 5; i++) {
+        let computerSelection = getComputerChoice();
+        let playerSelection = prompt("Choose your attack");
+        console.log(`Computer selected: ${computerSelection}`);
+        console.log(`Player selected: ${playerSelection}`);
+
+        let results = [];
+        currentLead = '';
+
+        results = playBall(playerSelection, computerSelection);
+
+        let resultsMsg = results[0];
+
+        if (results[1] === 'player') {
+            playerScore += 1;
+            console.log('Player score increased by one.');
+        } else if (results[1] === 'computer') {
+            compScore += 1;
+            console.log('Computer score increased by one.');
+        } else if (results[1] === 'tie') {
+            console.log('Tie, no change.');
+        }
+
+        if (playerScore > compScore) {
+            currentLead = 'player';
+        } else if (compScore > playerScore) {
+            currentLead = 'computer';
+        } else {
+            currentLead = 'tie'
+        }
+    }  
+
+    //track current leader/winner
+
+    if (currentLead === 'tie') {
+            return("No one wins.");
+        } else {
+            return(`The victor is the ${currentLead}!`);
+        }
+
+}
+
+alert(playThrough());
+*/
